24dc50d2915b07a9a821cc6bd8d308e4feb504eb
Abdirizak8899 <102373033+Abdirizak8899@users.noreply.github.com>
2022-03-25 20:10:14 +0300

    Add files via upload

---
+++ Simplecal.py
@@ -0,0 +1,13 @@
+# python simple calculator
+
+f = int(input("Enter 1st number: "))
+operator = input("choose the operator [+][-][×][÷]")
+s = int(input("Enter 2nd number: "))
+if operator == '+':
+    print("Answer is:" , int(f) + int(s))
+elif operator == '-':
+    print('Answer is:' , int(f) - int(s))
+elif operator == '*' or '×':
+    print('Answer is:' , int(f) * int(s))
+else:
+    print('Answer is:' , int(f) * int(s))
